43bb7ee74c8e036c53bea1bc4a19016df1fbe7d8
emrahxman <user@192.168.1.33>
2014-09-15 21:34:25 +0300

    emrah 2

--- src/com/sample/livewallpaper/bokehrainbow/AnimationWallpaper.java
+++ src/com/sample/livewallpaper/bokehrainbow/AnimationWallpaper.java
@@ -11,7 +11,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. and Emrah
+ * limitations under the License. and Emrah x 2
  */
 package com.sample.livewallpaper.bokehrainbow;
 
